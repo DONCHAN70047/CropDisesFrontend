0709ad06dbc64f83be644be83244f6e8e0b2a0f3
DONCHAN <arpanpatra800188500@gmail.com>
2025-11-27 00:30:37 +0530

    Utils connected to mongo problem solve  for my mistake

--- app/utils/mongo/connect_to_mongo.js
+++ app/utils/mongo/connect_to_mongo.js
@@ -1,20 +1,9 @@
 import mongoose from "mongoose";
 
 export async function connect_to_mongo() {
-  try {
-    if (mongoose.connection.readyState === 1) {
-      return;
-    }
+  const mongoURL = process.env.MONGO_URL
 
-    const mongoURL = process.env.MONGO_URL;
-
-    await mongoose.connect(mongoURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-  }
+  await mongoose.connect(`${mongoURL}`)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 }
