351dddb2dc00cd3d2911dc51a75687a772f9691f
DONCHAN <arpanpatra800188500@gmail.com>
2025-12-31 16:37:10 +0530

    Media set for image store 1.1...

--- app/page.js
+++ app/page.js
@@ -1,9 +1,30 @@
 "use client";
 import Link from "next/link";
+import { useEffect } from "react";
 import "./page.css";
-
 import RiceDiseaseCards from "./components/RiceDiseaseCards";
+
 export default function Home() {
+  useEffect(() => {
+    const callBackend = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/run-task/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ msg: "frontend triggered" }),
+        });
+        const data = await response.json();
+        console.log("Backend response:", data);
+      } catch (err) {
+        console.error("API call failed:", err);
+      }
+    };
+
+    callBackend();
+  }, []);
+
   return (
     <>
       <main className="container">
@@ -20,14 +41,15 @@ export default function Home() {
           <div className="buttonGroup">
             <Link href="/findout-disease" className="primaryBtn">
               <button className="primaryBtn">
-                Upload Leaf Image & Find Disease</button></Link>
+                Upload Leaf Image & Find Disease
+              </button>
+            </Link>
 
             <button className="secondaryBtn">Learn More</button>
           </div>
         </div>
-
-
       </main>
+
       {/* INFO SECTION */}
       <RiceDiseaseCards />
     </>
